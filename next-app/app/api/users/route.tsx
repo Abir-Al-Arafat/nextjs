9cb827a73698bc6329464016b8244a6bb767e9da
Abir-Al-Arafat <antorabir@gmail.com>
2024-04-30 19:55:20 +0600

    zod: validation added

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
@@ -15,11 +16,10 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  if (!body.name) {
-    return NextResponse.json(
-      { error: "name should not be empty" },
-      { status: 400 }
-    );
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
+
   return NextResponse.json(body, { status: 201 });
 }
